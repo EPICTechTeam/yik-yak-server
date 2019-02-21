22acbb2e39eb0ae57fc58e6bffc31cf45abc20c9
Andrew Thomas <andrewmthomas87@gmail.com>
2019-02-21 15:59:44 -0600

    Adds basic posts API

--- server.js
+++ server.js
@@ -3,10 +3,32 @@ const cors = require('cors')
 
 const app = express()
 
+const posts = [
+	{
+		body: 'Hello world',
+		date: '2/17/19'
+	},
+	{
+		body: 'Brutal and Extended Cold Blast could shatter ALL RECORDS - Whatever happened to Global Warming?',
+		date: '2/16/19'
+	},
+	{
+		body: 'nice',
+		date: 'nice'
+	}
+]
+
 app.use(cors())
 
-app.get('/', (request, response) => {
-	response.send('' + Math.random())
+app.get('/posts', (request, response) => {
+	response.json(posts)
+})
+
+app.get('/add-post', (request, response) => {
+	posts.push({
+		body: '' + Math.random(),
+		date: 'just now'
+	})
 })
 
 app.listen(7777, () => {
